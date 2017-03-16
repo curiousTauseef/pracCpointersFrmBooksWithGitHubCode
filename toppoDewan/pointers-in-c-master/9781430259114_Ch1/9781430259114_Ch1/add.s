1702def5a9ba121b8a361cd854496eaef90316ec
curiousTauseef <radiumtau@gmail.com>
2017-03-16 10:25:13 +0100

    Added YungLu

---
+++ toppoDewan/pointers-in-c-master/9781430259114_Ch1/9781430259114_Ch1/add.s
@@ -0,0 +1,52 @@
+	.file	"add.c"
+	.intel_syntax noprefix
+	.text
+	.globl	add
+	.type	add, @function
+add:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR [rbp-4], edi
+	mov	DWORD PTR [rbp-8], esi
+	mov	edx, DWORD PTR [rbp-4]
+	mov	eax, DWORD PTR [rbp-8]
+	add	eax, edx
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add, .-add
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 32
+	mov	DWORD PTR [rbp-20], edi
+	mov	QWORD PTR [rbp-32], rsi
+	mov	DWORD PTR [rbp-12], 10
+	mov	DWORD PTR [rbp-8], 20
+	mov	esi, 20
+	mov	edi, 10
+	call	add
+	mov	DWORD PTR [rbp-4], eax
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
